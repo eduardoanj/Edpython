a777ee20d5940a73e13ff344e11d058d932ebed6
Edu <eduardo_anjos15@yahoo.com>
2019-09-30 13:19:31 -0300

    tô aprendenu

--- py002opc.py
+++ py002opc.py
@@ -4,11 +4,12 @@ print('='*60)
 salario = float(input('{}Digite o salário: '.format(' '*5)))
 
 ####################################### A conta pode ser feita no format
-print('{}o salario liquido é: {}R${:.2f},'.format(' '*5,' '*9, salario))
-print('{}Os gastos E: {}R${:.2f}'.format(' '*5, ' '*17, salario*0.5 ))
-print('{}Investimentos LP: {}R${:.2f}'.format(' '*5, ' '*12, salario *0.2))
-print('{}Investimentos CP: {}R${:.2f}'.format(' '*5, ' '*12, salario *0.1))
-print('{}Livre: {}R${:.2f}'.format(' '*5, ' '*23, salario *0.2))
+###################################o 8 do {:8.2f} serve para a formatação
+print('{}o salario liquido é: {}R${:8.2f},'.format(' '*5,' '*9, salario))
+print('{}Os gastos E: {}R${:8.2f}'.format(' '*5, ' '*17, salario*0.5 ))
+print('{}Investimentos LP: {}R${:8.2f}'.format(' '*5, ' '*12, salario *0.2))
+print('{}Investimentos CP: {}R${:8.2f}'.format(' '*5, ' '*12, salario *0.1))
+print('{}Livre: {}R${:8.2f}'.format(' '*5, ' '*23, salario *0.2))
 
 
 ''
