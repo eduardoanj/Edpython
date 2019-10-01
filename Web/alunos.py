ab152c54c2e3cb55d8a48c9e6e261a87457c0a9a
Edu <eduardo_anjos15@yahoo.com>
2019-10-01 17:04:21 -0300

    webis

--- Web/alunos.py
+++ Web/alunos.py
@@ -1,7 +1,11 @@
 class Alunos:
-    def __init__(self, nome, telefone):
+    #o self deve sempre ser adicionado quando está dentro de uma classe
+    def __init__(self, nome, sobrenome, telefone):
         self.Nome = nome
+        #adição da variavel sobrenome
+        self.Sobrenome = sobrenome
         self.Telefone = telefone
-
-
-
+    def nome_completo(self):
+    #concatenação de strings tambem pode ser feita desta forma +''+
+        nomecompleto = self.Nome +' '+ self.Sobrenome
+        return nomecompleto    
